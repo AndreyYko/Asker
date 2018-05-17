3c7b57635a889c5aefcf631edca7587219da82d3
flackery <andrushagalushka@gmail.com>
2018-05-17 12:42:44 +0300

    Front-end v.1

---
+++ js/common.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+	$(".header-nav-mobile-version-visible-button").on('click', function() {
+		if ($(this).hasClass('open')) {
+			$(this).removeClass('open');
+			$(".header-nav-mobile-version > ul").slideToggle();
+		} else {
+			$(this).addClass('open');
+			$(".header-nav-mobile-version > ul").slideToggle();
+		}
+	});
+
+	$("#pop-up-button-yes").on('click', function() {
+		$("#buttons-visible").hide();
+		$("#pop-up-h3").html("Enter your answer");
+		$("#pop-up-form").fadeIn();
+		$("#pop-up-section").animate({
+			scrollTop: document.querySelector('.pop-up').scrollHeight
+		}, 500);
+	});
+
+	if ($(window).width() <= 768) {
+		$("#inputs-first").append("<div class='line-under-input'></div>");
+	}
+
+	$("#send-qustion-button").on('click', function() {
+		$(".pop-up").css('display', 'flex')
+    .hide()
+    .fadeIn();
+		return false;
+	});
+
+	$("#pop-up-button-no").on('click', function() {
+		$(".pop-up").css('display', 'none');
+	});
+
+	$("#pop-up-button-cancel").on('click', function() {
+		$(".pop-up").css('display', 'none');
+	});
+});
